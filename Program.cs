a12ace6bfc242bbd01742a381f3eb8d0911f1b02
Cozroth <t_ott_e@hotmail.com>
2023-09-21 13:57:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TryParse_Exercise
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+      Exempel4("Skriv en siffra: ");
+
+
+    }
+
+
+
+
+
+
+    public static void Exempel4(string question)
+    {
+      // Skapar en oändlig loop för att kunna fortsätta fråga användaren tills vi får
+      // en input vi kan konvertera till en integer.
+      while (true)
+      {
+        // Vad gör vi nu?
+
+
+
+
+      }
+
+
+    }
+  }
+}
